da22590138763e5cce2a334c718638dfb09591ec
Bokyeom <79684339+k-kbk@users.noreply.github.com>
2024-04-10 16:49:53 +0900

    [chore] mysql 및 docker-compose 설정 (#4)
    * [chore] 기타 수정
    
    * [chore] 배포 테스트
    
    * [chore] ssh-action 버전 수정
    
    * [chore] actions 버전 수정
    
    * [chore] 배포 테스트
    
    * [chore] 기타 수정
    
    * [chore] 기타 수정
    
    * [chore] 기타 수정
    
    * [chore] 기타 수정
    
    * [chore] cors 설정
    
    * [style] 개행문자 추가
    
    * [chore] 기타 수정
    
    * [chore] mysql 및 docker-compose 설정
    
    * [chore] dev 서버 배포 설정 변경
    
    * [chore] docker 옵션 수정
    
    * [chore] db url 수정
    
    * [chore] db url 수정
    
    * [chore] Dockerfile 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.palantir.docker-compose") version "0.36.0"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
 }
@@ -21,6 +22,7 @@ repositories {
 dependencies {
     // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     // kotlin
@@ -32,6 +34,9 @@ dependencies {
 
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // database
+    runtimeOnly("com.mysql:mysql-connector-j")
 }
 
 tasks.withType<KotlinCompile> {
