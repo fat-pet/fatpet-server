89b2ca829fd7a580972cb1f35ec7bf5a18002e3b
k-kbk <kkbk0077@gmail.com>
2024-04-13 12:52:54 +0900

    [style] 줄바꿈

--- src/main/kotlin/com/example/fatpetserver/common/ApiResponse.kt
+++ src/main/kotlin/com/example/fatpetserver/common/ApiResponse.kt
@@ -4,6 +4,7 @@ data class ApiResponse<T>(
     val message: String? = null,
     val body: T? = null,
 ) {
+    
     companion object {
         fun error(message: String?): ApiResponse<Unit> = ApiResponse(message = message)
 
--- src/main/kotlin/com/example/fatpetserver/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/fatpetserver/common/config/SecurityConfig.kt
@@ -12,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+    
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
