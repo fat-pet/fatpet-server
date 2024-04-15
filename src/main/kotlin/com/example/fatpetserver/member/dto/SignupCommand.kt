16cb64a6f26a5ef8a15c175efc2da640d2d5e2ff
k-kbk <kkbk0077@gmail.com>
2024-04-15 16:50:44 +0900

    [chore] dto 설명 및 예시 추가

--- src/main/kotlin/com/example/fatpetserver/member/dto/SignupCommand.kt
+++ src/main/kotlin/com/example/fatpetserver/member/dto/SignupCommand.kt
@@ -1,22 +1,27 @@
 package com.example.fatpetserver.member.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import org.hibernate.validator.constraints.Length
 
 data class SignupCommand(
+    @Schema(description = "이메일", example = "email@email.com")
     @field:NotBlank(message = "이메일은 필수 입력값입니다.")
     @field:Email(message = "이메일 형식이 올바르지 않습니다.")
     val email: String = "",
 
+    @Schema(description = "아이디", example = "loginId")
     @field:NotBlank(message = "아이디는 필수 입력값입니다.")
     @field:Length(min = 4, max = 12, message = "아이디는 4글자, 이상 12글자 이하여야 합니다.")
     val loginId: String = "",
 
+    @Schema(description = "비밀번호", example = "password")
     @field:NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @field:Length(min = 6, max = 16, message = "비밀번호는 6글자, 이상 16글자 이하여야 합니다.")
     val password: String = "",
 
+    @Schema(description = "닉네임", example = "nickname")
     @field:NotBlank(message = "닉네임은 필수 입력값입니다.")
     @field:Length(min = 2, max = 10, message = "닉네임은 2글자, 이상 10글자 이하여야 합니다.")
     val nickname: String = "",
