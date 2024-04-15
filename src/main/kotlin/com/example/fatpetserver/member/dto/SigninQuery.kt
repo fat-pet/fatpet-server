4c281d900d6499f0c0f5e399ae76e16ce9b1e460
k-kbk <kkbk0077@gmail.com>
2024-04-16 01:11:20 +0900

    [chore] 기타 수정

--- src/main/kotlin/com/example/fatpetserver/member/dto/SigninQuery.kt
+++ src/main/kotlin/com/example/fatpetserver/member/dto/SigninQuery.kt
@@ -4,11 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 
 data class SigninQuery(
-    @Schema(description = "아이디", example = "loginId")
+    @Schema(example = "loginId")
     @field:NotBlank(message = "아이디는 필수 입력값입니다.")
     val loginId: String = "",
 
-    @Schema(description = "비밀번호", example = "password")
+    @Schema(example = "password")
     @field:NotBlank(message = "비밀번호는 필수 입력값입니다.")
     val password: String = "",
 )
