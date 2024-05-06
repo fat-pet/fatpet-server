c39aac8d70ad126de8319f95cdb46238d6524d80
k-kbk <kkbk0077@gmail.com>
2024-05-06 14:38:46 +0900

    [chore] swagger 토큰 기능 추가

--- src/main/kotlin/com/example/fatpetserver/common/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/fatpetserver/common/config/SwaggerConfig.kt
@@ -3,6 +3,8 @@ package com.example.fatpetserver.common.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -12,7 +14,20 @@ class SwaggerConfig {
 
     @Bean
     fun swaggerApi(): OpenAPI = OpenAPI()
-        .components(Components())
+        .components(
+            Components().addSecuritySchemes(
+                "JWT",
+                SecurityScheme()
+                    .name("Authorization")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+            )
+        )
+        .addSecurityItem(
+            SecurityRequirement()
+                .addList("JWT")
+        )
         .info(
             Info()
                 .title("Fatpet API Documentation")
