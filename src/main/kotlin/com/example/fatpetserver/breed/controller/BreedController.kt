09cff200b71ac43327bca76c6f3bcac430e16b92
k-kbk <kkbk0077@gmail.com>
2024-05-21 01:08:29 +0900

    [style] 기타 수정

--- src/main/kotlin/com/example/fatpetserver/breed/controller/BreedController.kt
+++ src/main/kotlin/com/example/fatpetserver/breed/controller/BreedController.kt
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("/api/breeds")
 class BreedController(
